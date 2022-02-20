b1ecf2c763748b8744afb6b3373e38d9992256cc
Jerry <velasquezjerry98@gmail.com>
2022-02-19 19:08:00 -0800

    might throw this away, trying to make a simple navbar, gonna restart just to make this all cleaner

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/index.css'
+
+const Buttons = () => {
+  return (
+    <>
+      <div class="center">
+        <button class="button-1">Content 1</button>
+        <button class="button-2">Content 2</button>
+      </div>
+    </>
+  )
+}
+
 const Header = () => {
   return (
     <>
       <h1>Hello World!</h1>
       <p>This was made in React</p>
+      <p>I'm the first content btw</p>
     </>
   );
 }
@@ -13,8 +26,7 @@ const Header = () => {
 const SumnElse = () => {
   return (
     <>
-      <div class="uh">lorem something something
-       this my first react app ay</div>
+      <div class="uh">Hello, I'm the second content</div>
     </>
   )
 }
@@ -24,6 +36,7 @@ const SumnElse = () => {
 function Page() {
   return (
     <>
+      <Buttons/>
       <Header/>
       <SumnElse/>
     </>
